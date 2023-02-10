b69186b18ba5c971be61b459cf60385be41fa5ef
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-02-10 16:13:41 -0500

    make test specs pass

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,3 +1,14 @@
+class Card
+    attr_accessor :type
+
+    def initialize(type)
+        @type = type
+    end
+    
+end
+
+
+
 RSpec.describe 'Card' do 
     it 'has a type' do
         card = Card.new('Ace of Spades')
--- spec/first_assignment_spec.rb
+++ spec/first_assignment_spec.rb
@@ -6,20 +6,3 @@ RSpec.describe "math calculations" do
         expect(10/2).to eq(5)
     end
 end
-
-
-=begin
-     
-Create an example group with a string argument of "math calculations".
-
-Inside the group, create an example with a string argument of "does basic math".
-
-Inside the example, write 4 mathematical assertions of your choice.
-
-The expect method should receive a valid mathematical expression (for example, 3 + 4 or 5 * 3).
-
-The eq method should compare the result fo the evaluation with the right answer.
-
-
-    
-end
