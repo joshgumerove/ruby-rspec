14f686d1692fc89113affa923bf9f10f92438aa9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-02-10 15:31:33 -0500

    intro to it keyword

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,12 @@
 RSpec.describe 'Card' do 
-
+    it 'has a type' do
+        
+    end
 end
 
 
 
 # accepts a string as an argument
-# describe keyword --> creates an example group
+# describe keyword: creates an example group
+# it is a method: that goes inside the body of the block and also takes a block
+# should describe behaviour not technical implementation
