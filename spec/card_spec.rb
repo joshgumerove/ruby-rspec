39c51fb893418059a41e1a367f833f6c00d6bd69
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-02-11 01:49:22 -0500

    using the let keyword / memoization

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,5 +1,5 @@
 class Card
-    attr_reader :rank, :suit
+    attr_accessor :rank, :suit
 
     def initialize(rank, suit)
         @rank = rank
@@ -7,17 +7,13 @@ class Card
     end
 end
 
-RSpec.describe Card do 
-    def card 
-        Card.new("Ace", "Spades")
-    end
+RSpec.describe Card do
+    let(:card) { Card.new("Ace", "Spades")}
 
-    # before do
-    #     @card = Card.new('Ace', 'Spades')
-    # end
-
-    it 'has a rank' do
+    it 'has a rank and that rank can change' do
         expect(card.rank).to eq('Ace')
+        card.rank = "Queen"
+        expect(card.rank).to eq("Queen")
     end
 
     it 'has a suit' do
