0156199e3f5d2b020eac61e44f78ae839121c3c6
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-10 20:24:20 -0400

    decrease length test

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe "change matcher" do
     it 'checks that a method changes object state' do
         expect { subject.push(4) }.to change { subject.length }.by(1)
     end
+
+    it 'accepts negative arguments' do
+        expect { subject.pop }.to change { subject.length}.by(-1)
+    end
 end
