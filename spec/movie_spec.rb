9bac202a4fe7fbac73f232359684725f90a0e890
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-04-12 21:05:09 -0400

    movie class set-up

---
+++ spec/movie_spec.rb
@@ -0,0 +1,43 @@
+class Actor
+    attr_reader :name
+    def initialize(name)
+        @name = name
+    end
+
+    def ready?
+        sleep 3
+        true
+    end
+
+    def act 
+        "I love you, baby"
+    end
+
+    def fall_of_ladder
+        "Call my agent! No way!"
+    end
+
+    def light_on_fire
+        false
+    end
+end
+
+class Movie
+    attr_reader :actor
+    def initialize(actor)
+        @actor = actor
+    end
+
+    def start_shooting
+        if actor.ready?
+            puts actor.act
+            puts actor.fall_of_ladder
+            actor.light_on_fire
+        end
+    end
+end
+
+actor = Actor.new("Brad Pitt")
+movie = Movie.new(actor)
+puts movie.actor.name
+puts movie.start_shooting
